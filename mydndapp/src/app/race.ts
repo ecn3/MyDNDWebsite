521c79a86929f48cf572f704619416262cd836d1
ecn3 <43419606+ecn3@users.noreply.github.com>
2019-04-03 19:43:33 -0500

    added title feature to json

--- mydndapp/src/app/race.ts
+++ mydndapp/src/app/race.ts
@@ -2,6 +2,7 @@ export class Race {
     public name: string;
     public subraces: string;
     public publication: string;
+    public title: string;
     public page: number;
     public img: string;
  }
